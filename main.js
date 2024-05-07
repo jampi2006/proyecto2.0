e15f9cced34ccd67acf34b0354f8c9cfda8f05fc
jampi2006 <jamphierorellana641@gmail.com>
2024-05-06 22:48:16 -0600

    cree el login

---
+++ main.js
@@ -0,0 +1,20 @@
+function loguear()
+{
+let usuario = document.getElementById("usuario").value;
+let contraseña = document.getElementById("contraseña").value;
+let boton = document.getElementById("boton").value;
+
+if (usuario == "japy@gmial" && contraseña == "2022") {
+    window.location= "index.html";
+}
+
+else{
+ alert("datos invalidos")
+
+
+}
+
+}
+
+console.log(usuario. contraseña, boton);
+
