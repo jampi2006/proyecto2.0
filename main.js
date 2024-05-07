631d69d0b360cad526feea89ffc1096974bc7d14
jampi2006 <jamphierorellana641@gmail.com>
2024-05-07 09:17:51 -0600

    cambie de nombre los html agrege funsón

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ function loguear()
 {
 let usuario = document.getElementById("usuario").value;
 let contraseña = document.getElementById("contraseña").value;
-let boton = document.getElementById("boton").value;
 
-if (usuario == "japy@gmial" && contraseña == "2022") {
-    window.location= "index.html";
+if (usuario == "jamphier@gmail.com" && contraseña == "2020") {
+    window.location="index.html";
 }
 
-else{
- alert("datos invalidos")
+else
+{
+    alert("Datos incorrectos")
+}
 
 
 }
 
-}
-
-console.log(usuario. contraseña, boton);
 
