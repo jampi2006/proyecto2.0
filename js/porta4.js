a080d367fc1033dca3618b32789bff9876850712
jampi2006 <jamphierorellana641@gmail.com>
2024-05-12 19:25:00 -0600

    hice la funsión de cerrar seción

---
+++ js/porta4.js
@@ -0,0 +1,9 @@
+let input1 = document.getElementById("input1")
+
+let si = document.getElementById("si")
+
+let boton = document.getElementById('aggElements')
+
+boton.addEventListener("click" , function() {
+    input1.innerHTML = si.value
+})
